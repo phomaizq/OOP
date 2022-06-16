7d051c4868a214bfc2ac1edb9529103885c95fc7
Phomai <90522812+phomaizq@users.noreply.github.com>
2022-06-16 20:12:38 +0700

    Add files via upload

---
+++ New folder/nhanvieb1/Demo.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package nhanvieb1;
+
+/**
+ *
+ * @author hp
+ */
+import java.text.DecimalFormat;
+
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		DanhSachNV ds = new DanhSachNV();
+		ds.inputList();
+		ds.outputList();
+		
+//		System.out.println("Bang luong");
+//		ds.inLuong();
+//		
+//		 System.out.println("Tong luong nhan vien trong ds: " + ds.tinhTongLuong());
+//		DecimalFormat formatter = new DecimalFormat("0.00");
+//		System.out.println("Tong luong nhan vien trong ds: " + formatter.format(ds.tinhTongLuong()));            
+//		System.out.println("So luong tung nhan vien");
+//		ds.soLuongNV();
+	}
+
+}
